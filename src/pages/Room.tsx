6feb2b1133f9090f5c3f860809d5695cf03fc960
swapnil <swapnil130501@gmail.com>
2025-01-20 19:55:53 +0530

    Add user join room socket

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,7 +1,14 @@
+import { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { SocketContext } from "../context/SocketContext";
 
 const Room: React.FC = () => {
     const { id } = useParams();
+    const { socket } = useContext(SocketContext);
+    
+    useEffect(() => {
+        socket.emit('joined-room', { roomId: id });
+    }, [])
 
     return (
         <div>
