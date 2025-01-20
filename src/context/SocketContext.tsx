2b5995f585ead4a946247f6fce664ddd9ddc7565
swapnil <swapnil130501@gmail.com>
2025-01-20 21:17:45 +0530

    Fix error

--- src/context/SocketContext.tsx
+++ src/context/SocketContext.tsx
@@ -31,7 +31,7 @@ export const SocketProvider: React.FC<Props> = ({ children }) => {
 
     useEffect(() => {
         const userId = UUIDv4();
-        const newPeer = new Peer(userId. {
+        const newPeer = new Peer(userId, {
             host: "localhost",
             port: 9000,
             path: "/myapp"
